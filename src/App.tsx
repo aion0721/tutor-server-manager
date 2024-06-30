d8d5289a2b5151b8ff35a9f8d60160a74658613a
aion0721 <rudolf.aion@gmail.com>
2024-06-30 23:42:27 +0900

    style: nav, contents, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import "./App.css";
 import { Server } from "./Types/Server";
 import ServerTable from "./components/ServerTable";
@@ -22,9 +23,25 @@ const sampleServers: Server[] = [
 function App() {
   return (
     <>
-      <div>
-        <ServerTable servers={sampleServers}></ServerTable>
-      </div>
+      <Flex direction={"column"} minHeight={"100vh"}>
+        <Box as="nav" bg={"teal.500"} color={"white"} padding={"4"}>
+          <Text fontSize={"xl"} fontWeight={"bold"}>
+            Tutorial Server Manager
+          </Text>
+        </Box>
+        <Box flex={"1"} padding={"4"} bg={"gray.50"}>
+          <ServerTable servers={sampleServers}></ServerTable>
+        </Box>
+        <Box
+          as="footer"
+          bg={"gray.700"}
+          color={"white"}
+          padding={"4"}
+          textAlign={"center"}
+        >
+          <Text fontSize={"md"}>Copy rights</Text>
+        </Box>
+      </Flex>
     </>
   );
 }
